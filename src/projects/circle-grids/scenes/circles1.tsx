eb91c131e4d32a675ec2a520ddb33e28163f6f85
BarbarianBunny <86534881+BarbarianBunny@users.noreply.github.com>
2024-03-26 22:18:09 -0700

    feat: Calc difference between outer dots, base width of pattern, and start calc of outer dot positions

--- src/projects/circle-grids/scenes/circles1.tsx
+++ src/projects/circle-grids/scenes/circles1.tsx
@@ -1,11 +1,102 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { Circle, Layout, Line, makeScene2D } from "@motion-canvas/2d";
 import {
+  Logger,
+  Vector2,
+  createRef,
+  createRefArray,
   useLogger,
 } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
   //#region variables
   const logger = useLogger();
-  
+  const circle1 = new CirclePattern(2, logger);
+  logger.info(circle1.outerDotDiffs.toString());
   //#endregion
 });
+
+class CirclePattern {
+  // Containers
+  container = createRef<Layout>();
+  dotContainer = createRef<Layout>();
+  lineContainer = createRef<Layout>();
+  // Lists
+  outerDots = createRefArray<Circle>();
+  innerDots = createRefArray<Circle>();
+  public dots() {
+    return [...this.outerDots, ...this.innerDots];
+  }
+  lines = createRefArray<Line>();
+  // Values
+  size: number;
+  spacing: 10;
+  width: number;
+  outerDotDiffs: Vector2[];
+  // Debug
+  logger: Console | Logger;
+
+  public constructor(size: number, logger: Console | Logger) {
+    this.logger = logger;
+    this.size = size;
+    this.outerDotDiffs = this.calcOuterDotDiffs();
+    this.width = this.calcPatternWidth();
+    this.logger.info(this.width.toString());
+
+    <Layout ref={this.container}>
+      <Layout ref={this.dotContainer}></Layout>
+      <Layout ref={this.lineContainer}></Layout>
+    </Layout>;
+  }
+
+  // Return an array of pos differences between outer dots
+  // Starts at the top middle left dot
+  calcOuterDotDiffs(): Vector2[] {
+    const diffs = [new Vector2(this.size, 0)];
+    function lastDiff(): Vector2 {
+      return diffs[diffs.length - 1];
+    }
+
+    while (lastDiff().y < this.size) {
+      diffs.push(lastDiff().addY(1));
+    }
+    while (lastDiff().x > -this.size) {
+      diffs.push(lastDiff().addX(-1));
+    }
+    while (lastDiff().y > -this.size) {
+      diffs.push(lastDiff().addY(-1));
+    }
+    while (lastDiff().x < this.size) {
+      diffs.push(lastDiff().addX(1));
+    }
+    while (lastDiff().y < -1) {
+      diffs.push(lastDiff().addY(1));
+    }
+    diffs.pop();
+    return diffs;
+  }
+
+  // Returns the pattern's base width
+  // Based on the diff steps from rightest dot to leftest dot
+  calcPatternWidth(): number {
+    const x = this.outerDotDiffs.map(function (value) {
+      return value.x;
+    });
+    const negatives = x.filter(function (a) {
+      return a < 0;
+    });
+    const sum = negatives.reduce(function (a, b) {
+      return a + b;
+    });
+    return Math.abs(sum);
+  }
+
+  // Returns the pattern's outer dot's positions
+  // Accounts for spacing between dots
+  calcOuterDotPos(): Vector2[] {
+    const startX: number = Math.floor(this.size / 2) * this.spacing;
+    const startY: number = Math.floor(this.width / 2) * this.spacing;
+    const pos: Vector2[] = [new Vector2(startX, startY)];
+    // TODO:
+    return pos;
+  }
+}
